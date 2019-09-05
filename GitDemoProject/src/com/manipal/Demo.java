bb408ca316e6f44387a48dc0836ca20155163bb8
ITFS <ITFS@ITFS-17>
2019-09-05 11:27:59 +0530

    after added second line , save it in remote report

--- GitDemoProject/src/com/manipal/Demo.java
+++ GitDemoProject/src/com/manipal/Demo.java
@@ -4,7 +4,9 @@ public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-       System.out.println("hello git");
+       System.out.println("hello git.....");
+	System.out.println("second line added...");
 	}
+	
 
 }
